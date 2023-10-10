de4078d01803c28982d5dd9f2abaf63328303507
Perri Rodgers <pearrirodgers@gmail.com>
2023-10-10 06:11:11 -0600

    Created api-url file for setup api-endpoints

---
+++ src/common/store/api-url.js
@@ -0,0 +1,7 @@
+const BaseUrl = process.env.REACT_APP_BASE_URL;
+
+// for api url endpoints
+export const UnsplashApiUrl = {
+    GET_PHOTOS_API: BaseUrl + "/photos/",
+    SEARCH_PHOTOS_API: BaseUrl + "/search/photos"
+}
