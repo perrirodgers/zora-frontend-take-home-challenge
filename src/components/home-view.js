002d60aa8a93752352e6287c6526cb4e2a9a842e
Perri Rodgers <pearrirodgers@gmail.com>
2023-10-10 06:40:38 -0600

    designed filters toolbar

--- src/components/home-view.js
+++ src/components/home-view.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import { SearchBar } from "./searchBar";
+import { Dropdown } from "react-bootstrap";
 
 export const HomeView = () => {
-
+    // for search keyword
     const [searchKeyword, setSearchKeyword] = useState("");
+    // for orderby filter
+    const [orderBy, setOrderBy] = useState("Select Order");
+    // for color filter
+    const [color, setColor] = useState("Select Color");
 
     return (
         <>
@@ -11,8 +16,69 @@ export const HomeView = () => {
                 <h1 className="align-self-center text-center">Welcome to Unsplash</h1>
             </div>
             <div className="d-flex justify-content-center mt-5">
-                <SearchBar value={searchKeyword} setSearchKeyword={setSearchKeyword} onClick={() => { }} />
+                <SearchBar
+                    value={searchKeyword}
+                    setSearchKeyword={setSearchKeyword}
+                    onClick={() => { }}
+                />
+            </div>
+            <div className="dropdownContainer">
+                <Dropdown
+                    drop
+                    className="dropdown filtstatus"
+                    onSelect={(e) => {
+                        setOrderBy(e);
+                    }}
+                >
+                    <span className="sortingShow">
+
+                        <Dropdown.Toggle>
+                            <span>{orderBy}</span>
+                        </Dropdown.Toggle>
+
+                    </span>
+                    <Dropdown.Menu
+                        variant=""
+                        id="style-5"
+                        className="cusDrupdown"
+                    >
+                        <Dropdown.Item eventKey={"latest"} key={"latest"}>
+                            {"latest"}
+                        </Dropdown.Item>
+                        <Dropdown.Item eventKey={"relevant"} key={"relevant"}>
+                            {"relevant"}
+                        </Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Dropdown
+                    drop
+                    className="dropdown filtstatus"
+                    onSelect={(e) => {
+                        setColor(e);
+                    }}
+                >
+                    <span className="sortingShow">
+
+                        <Dropdown.Toggle>
+                            <span>{color}</span>
+                        </Dropdown.Toggle>
+
+                    </span>
+                    <Dropdown.Menu
+                        className="cusDrupdown"
+                    >
+                        <Dropdown.Item eventKey={"green"} key={"green"}>
+                            {"latest"}
+                        </Dropdown.Item>
+                        <Dropdown.Item eventKey={"red"} key={"red"}>
+                            {"red"}
+                        </Dropdown.Item>
+                        <Dropdown.Item eventKey={"blue"} key={"blue"}>
+                            {"blue"}
+                        </Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
             </div>
         </>
-    )
-}
+    );
+};
